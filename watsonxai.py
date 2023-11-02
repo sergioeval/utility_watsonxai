db55aa11613bfcabf0b3792e667b978b5d2c2a60
SERGIO ESTRADA VALENZUELA <sergioev@gmail.com>
2023-11-01 18:13:36 -0600

    Update watsonxai.py

--- watsonxai.py
+++ watsonxai.py
@@ -13,7 +13,11 @@ class WatsonxAi:
                 repetitionPenalty= 1,
                 decodingMethod='greedy',
                 minTokens=0,
-                maxTokens=50):
+                maxTokens=50, 
+                randomSeed=None,
+                temperature=0,
+                topK =50,
+                topP = 1,):
         self.modelId = modelId
         self.projectId = projectId
         self.cloudApiKey = cloudApiKey
@@ -22,6 +26,10 @@ class WatsonxAi:
         self.minTokens = minTokens
         self.stopSequences = stopSequences
         self.repetitionPenalty = repetitionPenalty
+        self.randomSeed = randomSeed
+        self.temperature = temperature
+        self.topK = topK
+        self.topP = topP
     
     def create_model(self):
         credentials = { 
@@ -29,15 +37,29 @@ class WatsonxAi:
             "apikey" : self.cloudApiKey
         }
 
-        gen_parms = { 
-            "DECODING_METHOD" : self.decodingMethod, 
-            "MIN_NEW_TOKENS" : self.minTokens, 
-            "MAX_NEW_TOKENS" : self.maxTokens,
-            "STOP_SEQUENCES" : self.stopSequences,
-            "REPETITION_PENALTY": self.repetitionPenalty
-        }
-
+        if self.decodingMethod == 'greedy':
+            gen_parms = { 
+                "DECODING_METHOD" : self.decodingMethod, 
+                "MIN_NEW_TOKENS" : self.minTokens, 
+                "MAX_NEW_TOKENS" : self.maxTokens,
+                "STOP_SEQUENCES" : self.stopSequences,
+                "REPETITION_PENALTY": self.repetitionPenalty
+            }
+        if self.decodingMethod == 'sample':
+            gen_parms = { 
+                "DECODING_METHOD" : self.decodingMethod, 
+                "MIN_NEW_TOKENS" : self.minTokens, 
+                "MAX_NEW_TOKENS" : self.maxTokens,
+                "STOP_SEQUENCES" : self.stopSequences,
+                "REPETITION_PENALTY": self.repetitionPenalty,
+                "RANDOM_SEED": self.randomSeed,
+                "TEMPERATURE": self.temperature,
+                "TOP_K": self.topK,
+                "TOP_P": self.topP
+            }
+            
         self.model = Model( self.modelId, credentials, gen_parms, self.projectId )
+        
         return self.model
 
     def run_prompt(self, prompt):
